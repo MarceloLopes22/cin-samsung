cdedc44173b570bc2153fd4a40a1b81e508fff5b
Marcelo Lopes <marcelomlopes2@gmail.com>
2019-09-25 01:02:36 -0300

    arquivo de orientação

--- Modelagem/Criação.sql
+++ Modelagem/Criação.sql
@@ -8,11 +8,11 @@ CREATE TABLE Equipamento (
   modelo VARCHAR(255) NOT NULL,
   mesano VARCHAR(20) NOT NULL,
   valor DECIMAL NOT NULL,
-  foto LONGBLOB NOT NULL,
+  foto LONGBLOB NULL,
   PRIMARY KEY(id_equipamento)
 );
 
-INSERT INTO `cin.Equipamento` (`id_equipamento`,`tipo`,`modelo`,`mesano`,`valor`,`foto`) VALUES (3,'ELETRONICO','casa','1900-01',1111111111,?);
-INSERT INTO `cin.Equipamento` (`id_equipamento`,`tipo`,`modelo`,`mesano`,`valor`,`foto`) VALUES (4,'AUTOMOTIVO','Fox Xtreme2019','2019-09',6000000000,?);
+INSERT INTO cin.Equipamento (id_equipamento,tipo,modelo,mesano,valor,foto) VALUES (1,'ELETRONICO','casa','1900-01',1111111111,null);
+INSERT INTO cin.Equipamento (id_equipamento,tipo,modelo,mesano,valor,foto) VALUES (2,'AUTOMOTIVO','Fox Xtreme2019','2019-09',6000000000,null);
 
 select * from cin.Equipamento;
